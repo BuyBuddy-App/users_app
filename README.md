9bde2091610c16fded7c3197354200907dd9d24e
3oraby <mohaahmed21102003@gmail.com>
2025-08-31 18:53:35 +0300

    create app

---
+++ README.md
@@ -0,0 +1,3 @@
+# buy_buddy_user_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/buy_buddy_user_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.buy_buddy_user_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
