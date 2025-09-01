f70c3837806bea909e2eb80e9cb44ff116914541
3oraby <mohaahmed21102003@gmail.com>
2025-09-01 04:31:12 +0300

    add utils

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,19 @@
+class AppStrings {
+  static const String appTitle = "WhatsApp Clone";
+
+  static const String fieldIsRequired = "Field is required";
+  static const String emailIsRequired = "Email is required";
+  static const String invalidEmail = "Please enter a valid email address";
+  static const String passwordIsRequired = "Password is required";
+  static const String passwordTooShort =
+      "Password must be at least 8 characters long";
+  static const String phoneIsRequired = "Phone number is required";
+  static const String invalidPhone = "Please enter a valid phone number";
+  static const String ageIsRequired = "Age is required";
+  static const String invalidAge = "Please enter a valid age";
+  static const String ageTooYoung = "Age must be 14 or older";
+  static const String ageTooOld = "Please enter a realistic age";
+  static const String confirmPasswordIsRequired =
+      "Confirm password is required.";
+  static const String passwordsDoNotMatch = "Passwords do not match.";
+}
