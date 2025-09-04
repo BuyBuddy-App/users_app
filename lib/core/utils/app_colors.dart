c15c318ac2099de5572ec10820438a356471f868
3oraby <mohamedahmed21102003@gmail.com>
2025-09-04 17:12:58 +0300

    add const for each color

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primary = Colors.black;
-  static Color onPrimary = Colors.white;
-  static Color buttonBackground = Color(0xff222222);
-  static Color focusBorder = Color(0xff8770C2);
-  static List<Color> gradColors = [
+  static const Color primary = Colors.black;
+  static const Color onPrimary = Colors.white;
+  static const Color buttonBackground = Color(0xff222222);
+  static const Color focusBorder = Color(0xff8770C2);
+  static const List<Color> gradColors = [
     Color(0xffFFCC60),
     Color(0xffAC8DD8),
     Color(0xff7F5EAD),
