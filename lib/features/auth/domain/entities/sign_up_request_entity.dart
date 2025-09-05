791ea7200e1abfc276c15554637d31ab78057962
3oraby <mohamedahmed21102003@gmail.com>
2025-09-05 06:18:20 +0300

    sign up logic

---
+++ lib/features/auth/domain/entities/sign_up_request_entity.dart
@@ -0,0 +1,13 @@
+class SignUpRequestEntity {
+  final String name;
+  final String email;
+  final String password;
+  final String userName;
+
+  const SignUpRequestEntity({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.userName,
+  });
+}
