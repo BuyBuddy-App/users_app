97aabec76b04b20a9ce6061699ef58f3ea1f7fb1
3oraby <mohamedahmed21102003@gmail.com>
2025-09-06 06:39:50 +0300

    endpoints url

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,6 +1,11 @@
 class EndPoints {
   // Auth
-  static const String getRefreshToken = "auth/refreshToken";
+  static const String refreshToken = "auth/refreshToken";
   static const String login = "auth/login";
-  static const String signUp = "auth/signUp";
+  static const String signUp = "auth/signup";
+  static const String logout = "auth/logout";
+  static const String verifyEmail = "auth/verify-email";
+  static const String resetPassword = "auth/reset-password";
+  static const String verifyResetCode = "auth/verify-reset-code";
+  static const String forgetPassword = "auth/forget-password";
 }
